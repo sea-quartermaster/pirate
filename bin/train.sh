917e4f4d71b730a9a963514f1c55d9a47593afd0
Andrew Hood <andrewhood125@gmail.com>
2019-01-28 15:18:00 -0800

    Update training script to skip ci

--- bin/train.sh
+++ bin/train.sh
@@ -3,6 +3,6 @@
 for i in `seq 1 30`; do
     touch "train/${i}"
     git add train/
-    git commit --message "Automatic Training"
+    git commit --message "[ci skip] Automatic Training ${i}"
     bundle exec rspec
 done
