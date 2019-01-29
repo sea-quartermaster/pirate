eac0bc502c7a133065d8290b3de8dc4f581d9c71
Andrew Hood <andrewhood125@gmail.com>
2019-01-28 16:05:25 -0800

    Updating the training script

--- bin/train.sh
+++ bin/train.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+rm train/*
+
 for i in `seq 1 30`; do
     touch "train/${i}"
     git add train/
     git commit --message "[ci skip] Automatic Training ${i}"
+    git push
     bundle exec rspec
 done
