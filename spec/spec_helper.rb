bbd2aff05589e5efe4bf78cbd32aaa4ee7688088
Andrew Hood <andrewhood125@gmail.com>
2019-04-13 22:50:33 -0700

    Bump look_out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'dotenv/load'
 require 'pry'
 require 'look_out'
 LookOut.configure do |config|
-  #config.base_uri = 'localhost:3001/v1'
-  config.api_key = ENV['FIRST_MATE_API_KEY']
+  config.first_mate_api_key = ENV['FIRST_MATE_API_KEY']
+  config.red_cove_api_key = ENV['RED_COVE_API_KEY']
   config.user = `git config user.name`.chomp
 end
 
